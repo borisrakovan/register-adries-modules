18e25acb6ce6ebced977bce20ed6daa56d9dd4cc
borisrakovan <b.rakovan@gmail.com>
2020-04-19 13:55:42 +0200

    initial commit

---
+++ register-adries-fix/src/main/java/ra/RequestBuilder.java
@@ -0,0 +1,72 @@
+package ra;
+
+import javax.json.*;
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * Used by to build a json dictionary from given parameters
+ */
+public class RequestBuilder {
+    public RequestBuilder() {
+    }
+
+    public String buildDatastoreRequest(Map<String, String> params, JsonArray records){
+        JsonBuilderFactory factory = Json.createBuilderFactory(Collections.<String, Object>emptyMap());
+        JsonObjectBuilder jsonBuilder = factory.createObjectBuilder();
+        for(String key : params.keySet()){
+            jsonBuilder.add(key, params.get(key));
+        }
+
+        if(records != null){
+            jsonBuilder.add("records", records);
+        }
+        JsonObject request = jsonBuilder.build();
+        return request.toString();
+    }
+
+    public String buildJsonHttpRequest(Map<String, String> params){
+        JsonBuilderFactory factory = Json.createBuilderFactory(Collections.<String, Object>emptyMap());
+        JsonObjectBuilder jsonBuilder = factory.createObjectBuilder();
+        for(String key : params.keySet()){
+            jsonBuilder.add(key, params.get(key));
+        }
+        return jsonBuilder.build().toString();
+    }
+
+
+    public String buildDatastoreCreateWithFields(Map<String, String> params, Map<String, String> fields){
+        JsonBuilderFactory factory = Json.createBuilderFactory(Collections.<String, Object>emptyMap());
+        JsonObjectBuilder builder = factory.createObjectBuilder();
+        for(String key : params.keySet()){
+            builder.add(key, params.get(key));
+        }
+
+        if(fields != null && fields.size() > 0){
+            JsonArrayBuilder fieldsBuilder = factory.createArrayBuilder();
+            JsonObjectBuilder fieldBuilder;
+            for(String id : fields.keySet()){
+                fieldBuilder = factory.createObjectBuilder();
+                fieldBuilder.add("id", id);
+                fieldBuilder.add("type", fields.get(id));
+                fieldsBuilder.add(fieldBuilder.build());
+            }
+            builder.add("fields", fieldsBuilder.build());
+        }
+
+        return builder.build().toString();
+    }
+
+    /* used when initializing dataset in resource that doesn't exist yet, not in use right now */
+    public String buildDatastoreResourceCreate(Map<String, String> params){
+        JsonBuilderFactory factory = Json.createBuilderFactory(Collections.<String, Object>emptyMap());
+        JsonObjectBuilder childBuilder = factory.createObjectBuilder();
+        for(String key : params.keySet()){
+            childBuilder.add(key, params.get(key));
+        }
+        JsonObjectBuilder builder2 = factory.createObjectBuilder();
+        builder2.add("resource", childBuilder.build());
+
+        return builder2.build().toString();
+    }
+}
